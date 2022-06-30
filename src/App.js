c79e72bf888fa34208628e32eadccb1458144c63
F3RN4ND02 <fernando.ujap@gmail.com>
2022-06-29 22:41:55 -0400

    introducing login

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import Login from './Login';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice'
 
 function App() {
+const user = useSelector(selectUser)
+
   return (
     <div className="app">
       <Header/>
+{!user ? (
+  <Login/>
+) : (
       <div className="app_body">
         <Sidebar/>
         <Feed/>
-      </div>
+      </div> 
+    )}
     </div>
   );
 
